85fd27fe40f2dfa6f9f1fdba1e8a6abd4d7f9eb9
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-13 05:33:58 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,10 +4,7 @@
   def change
     create_table :costume_stores do |t|
       t.string :name
-      t.float :price 
-      t.string :image_url
-      t.string :size 
-      t.timestamps
+    
     end
   end
   
