72f33cde7e6a6d2942a3f177aa5022bef56d6c4f
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-13 05:30:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+ class CostumeStores < ActiveRecord::Migration[5.0]
+  
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.float :price 
+      t.string :image_url
+      t.string :size 
+      t.timestamps
+    end
+  end
+  
+end 
