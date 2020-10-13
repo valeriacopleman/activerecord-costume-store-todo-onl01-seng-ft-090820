c5aa0280cbf2c3af3a331a2613fe92825b416d47
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-13 05:28:38 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[5.0]
     create_table :costumes do |t|
       t.string :name
       t.float :price 
-      t.string :image
+      t.string :image_url
       t.string :size 
       t.timestamps
     end
