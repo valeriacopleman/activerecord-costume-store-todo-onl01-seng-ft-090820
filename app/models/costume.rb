ff148bcacd1e25c91619885eb4f8f693a8888fc1
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-13 04:56:26 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -4,6 +4,8 @@ class Costume << ActiveRecord::Base
     create_table :costume do |t|
       t.string :name 
       t.integer :price 
+      t.text :image_url
+      t.integer :size 
       
   end
   
