064bbf1d05aedb0374d56be2c11d0dc3e65b8deb
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-13 05:08:39 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,12 +1,5 @@
 class Costume < ActiveRecord::Migration[5.2]
   
-  def change
-    create_table :costume do |t|
-      t.string :name 
-      t.integer :price 
-      t.text :image_url
-      t.integer :size 
-    end
-  end
+
   
 end
